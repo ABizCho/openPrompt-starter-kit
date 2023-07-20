ce24c798953ceb72681fb2a7046c9c44cf804d25
jiheon788 <jiheon788@ajou.ac.kr>
2023-07-20 22:14:24 +0900

    feat: 동적 라우팅 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import '@/App.css';
-import logo from '@/logo.svg';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from '@/lib/queries/queryClient';
+import DynamicRoutes from './DynamicRoutes';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <DynamicRoutes />
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 };
 
