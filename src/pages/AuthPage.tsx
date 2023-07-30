279ea5b035afb2f83b653ff587f1644bcbdca267
jiheon788 <jiheon788@ajou.ac.kr>
2023-07-30 21:07:11 +0900

    feat: 커스텀훅

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,5 +1,37 @@
+import Template from '@/components/common/CustomUI/template';
+import { useInputs } from '@/lib/hooks/useInputs';
+
 const AuthPage = () => {
-  return <>Auth</>;
+  const [data, onChange, onInit] = useInputs<{
+    received: string;
+    amount: string;
+  }>({
+    received: '',
+    amount: '',
+  });
+
+  return (
+    <Template variant="gradient">
+      {data.received}
+      <input
+        type="text"
+        name="received"
+        placeholder="받을 사람"
+        value={data.received}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="amount"
+        placeholder="보낼 돈"
+        value={data.amount}
+        onChange={onChange}
+      />
+      <button type="button" onClick={onInit}>
+        onInit
+      </button>
+    </Template>
+  );
 };
 
 export default AuthPage;
